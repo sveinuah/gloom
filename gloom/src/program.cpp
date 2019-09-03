7befd84069ceef183f7f04af43d1bbb5c6059944
Sveinung Haugane <sveihaug@gmail.com>
2019-09-03 15:33:04 +0200

    Added Buffer setup and rendered first triangle

--- gloom/src/program.cpp
+++ gloom/src/program.cpp
@@ -1,6 +1,8 @@
 // Local headers
 #include "program.hpp"
 #include "gloom/gloom.hpp"
+#include "gloom/shader.hpp"
+#include "Buffer.h"
 
 
 void runProgram(GLFWwindow* window)
@@ -17,6 +19,49 @@ void runProgram(GLFWwindow* window)
 
     // Set up your scene here (create Vertex Array Objects, etc.)
 
+	uint32_t va_id = 0;
+	glCreateVertexArrays(1, &va_id);
+	glBindVertexArray(va_id);
+
+	float vertices[3 * 3]
+	{
+		-0.6,	-0.6,	0,
+		0.6,	-0.6,	0,
+		0,		0.6,	0
+	};
+
+	BufferLayout layout
+	{
+		{"position", DataType::Float3}
+	};
+	VertexBuffer vertexbuffer{ vertices, sizeof(vertices), layout };
+
+	vertexbuffer.Bind();
+	uint32_t index = 0;
+	for (const auto& element : layout)
+	{
+		glEnableVertexAttribArray(index);
+		glVertexAttribPointer(
+			index,
+			element.GetElementCount(),
+			ToGLType(element.type),
+			element.normalized ? GL_TRUE : GL_FALSE,
+			layout.GetStride(),
+			(const void*)element.offset
+		);
+		index++;
+	}
+
+	uint32_t indicies[3]{ 0, 1, 2 };
+	IndexBuffer indexbuffer{ indicies, sizeof(indicies) / sizeof(uint32_t) };
+	indexbuffer.Bind();
+
+	Gloom::Shader shader;
+	shader.makeBasicShader(
+		R"(C:\Users\Sveinung\Source\Repos\gloom\gloom\shaders\simple.vert)",
+		R"(C:\Users\Sveinung\Source\Repos\gloom\gloom\shaders\simple.frag)"
+	);
+
     // Rendering Loop
     while (!glfwWindowShouldClose(window))
     {
@@ -24,6 +69,13 @@ void runProgram(GLFWwindow* window)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Draw your scene here
+		shader.activate();
+		glBindVertexArray(va_id);
+		vertexbuffer.Bind();
+		indexbuffer.Bind();
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		shader.deactivate();
+
 
         // Handle other events
         glfwPollEvents();
