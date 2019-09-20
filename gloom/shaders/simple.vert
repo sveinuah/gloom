4e14e07f45014e547e2beb5ea09f040105722625
Sveinung Haugane <sveihaug@gmail.com>
2019-09-20 19:55:15 +0200

    Assignment 2 complete

--- gloom/shaders/simple.vert
+++ gloom/shaders/simple.vert
@@ -3,10 +3,12 @@
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec4 color;
 
+layout(location = 2) uniform mat4 mvp = mat4(1.0);
+
 out vec4 v_Color;
 
 void main()
 {
 	v_Color = color;
-    gl_Position = vec4(position, 1.0f);
+    gl_Position = mvp * vec4(position, 1.0f);
 }
--- gloom/src/program.cpp
+++ gloom/src/program.cpp
@@ -1,14 +1,16 @@
 // Local headers
 #include "program.hpp"
+#include <glm/glm.hpp>
 #include "gloom/gloom.hpp"
 #include "gloom/shader.hpp"
+#include <chrono>
 
 // Made a header file to hold the diferent buffers used in the assignment
 #include "TaskBuffers.h"
 
 std::vector<float> fillVertexBuffer(const std::vector<float>& positions, const std::vector<float>& colors)
 {
-	assert(positions.size() / 3 == colors.size() / 4); //Assert if there are different amounts of vvertices
+	assert(positions.size() / 3 == colors.size() / 4); //Assert if there are different amounts of vertices
 
 	const uint32_t num_elems = positions.size() / 3;
 	
@@ -81,24 +83,72 @@ void runProgram(GLFWwindow* window)
 
     // Set up your scene here (create Vertex Array Objects, etc.)
 
-	uint32_t va_id = CreateTriangleVAO(TaskTwo::positions, TaskTwo::colors, TaskTwo::indices);
+	uint32_t va_id = CreateTriangleVAO(TaskThree::positions, TaskThree::colors, TaskThree::indices);
 
 	Gloom::Shader shader;
 	shader.makeBasicShader(
-		R"(C:\Users\Sveinung\Source\Repos\gloom\gloom\shaders\simple.vert)",
-		R"(C:\Users\Sveinung\Source\Repos\gloom\gloom\shaders\simple.frag)"
+		R"(..\..\..\gloom\shaders\simple.vert)",
+		R"(..\..\..\gloom\shaders\simple.frag)"
 	);
+
+	// Uniform for ModelViewProjection matrix
+	glm::mat4x4 scale{1.0f};
+	glm::mat4x4 rotation{1.0f};
+	glm::mat4x4 translation{1.0f};
+
+	const float pi = 3.141592f;
+	float scale_angle = 0.0f;
+	float rotation_angle = 0.0f;
+	float translation_angle = 0.0f;
+
+	const float scale_speed = 1.3f;
+	const float rotation_speed = 0.7f;
+	const float translation_speed = 2.1f;
+
+	using namespace std::chrono;
+	using namespace std::chrono_literals;
+	using Seconds = std::chrono::duration<float, std::ratio<1>>;
+
+	auto last = steady_clock::now();
+	auto curr = last;
 
     // Rendering Loop
     while (!glfwWindowShouldClose(window))
     {
+		curr = steady_clock::now();
+		nanoseconds dt = (curr - last);
+		last = curr;
+
         // Clear colour and depth buffers
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Scaling
+
+		scale_angle += scale_speed * Seconds(dt).count();
+		scale[0][0] = 1 + 0.5 * std::sin(scale_angle);
+		scale[1][1] = 1 + 0.5 * std::sin(scale_angle);
+		scale[2][2] = 1 + 0.5 * std::sin(scale_angle);
+
+		// Angle calculations
+		rotation_angle += rotation_speed * Seconds(dt).count();
+		rotation[0][0] = std::cos(rotation_angle);
+		rotation[1][1] = std::cos(rotation_angle);
+		rotation[0][1] = -std::sin(rotation_angle);
+		rotation[1][0] = std::sin(rotation_angle);
+
+		// Translations
+
+		translation_angle += translation_speed * Seconds(dt).count();
+		translation[0][3] = std::sin(translation_angle);
+		translation[1][3] = std::cos(translation_angle);
+
 
         // Draw your scene here
 		shader.activate();
 		
 		glBindVertexArray(va_id);
+		auto mvp = scale * rotation * translation;
+		glUniformMatrix4fv(2, 1, GL_TRUE, (float*)&mvp);
 		glDrawElements(GL_TRIANGLES, 3 * 3, GL_UNSIGNED_INT, 0);
 
 		shader.deactivate();
